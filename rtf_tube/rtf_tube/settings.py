9151891c7dcc60faf2d0e4925ee90ae4ce18e903
Ramil Mavliutov <stirk2281337@gmail.com>
2023-04-17 17:28:39 +0500

    add icon and add postgresql

--- rtf_tube/rtf_tube/settings.py
+++ rtf_tube/rtf_tube/settings.py
@@ -8,11 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 ### S3 КОНСТАНТЫ
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
+AWS_ACCESS_KEY_ID = 'dYWyowxB1tpSxSDpezb3jM'
+AWS_SECRET_ACCESS_KEY = 'g2TXPDmnBLHaf7u8ZBcEFyK7V7J4sANXeFJxh9DRvnbU'
 AWS_STORAGE_BUCKET_NAME = 'rtf-tube'
 AWS_S3_ENDPOINT_URL = 'https://hb.bizmrg.com'
-STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}}
+#STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}}
 AWS_S3_CALLING_FORMAT = 'boto.s3.connection.OrdinaryCallingFormat'
 AWS_S3_CUSTOM_DOMAIN = '%s.hb.bizmrg.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION = 'static'
@@ -24,7 +24,7 @@ DEFAULT_FILE_STORAGE = 'rtf_tube.storage.MediaStorage'
 #MEDIA_ROOT = 'static/media'
 AWS_S3_REGION_NAME = 'ru-msk'
 
-
+PASSWORD_RESET_TIMEOUT = 259200
 
 ###
 
@@ -95,10 +95,21 @@ WSGI_APPLICATION = 'rtf_tube.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'baza',
+        'USER': 'admin',
+        'HOST': 'localhost',
+        'PASSWORD': 'admin',
+    
     }
 }
 
