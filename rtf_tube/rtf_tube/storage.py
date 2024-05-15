0b42a52f05010b32eb5ada9a8e73487ddd1e59e8
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2024-05-16 00:44:16 +0500

    Update storage.py

--- rtf_tube/rtf_tube/storage.py
+++ rtf_tube/rtf_tube/storage.py
@@ -2,5 +2,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = os.environ.get(settings.AWS_STORAGE_BUCKET_NAME)
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
     custom_domain = "storage.yandexcloud.net/{}".format(bucket_name)
